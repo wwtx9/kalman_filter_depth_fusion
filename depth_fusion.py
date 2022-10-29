9c162604df5d40385cdfed6e91484651fdd69c1f
wwtx9 <leagueapdow@gmail.com>
2022-10-28 22:41:31 -0400

    virtual kitti2 version

--- depth_fusion.py
+++ depth_fusion.py
@@ -16,10 +16,12 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-r", "--src_folder",
                         help="path to pipeline's depth maps", type=str, default="")
-    parser.add_argument("-r2", "--src_poses_path",
-                        help="path to pipeline's poses", type=str, default="")
-    parser.add_argument("-n", "--dataset_name",
-                        help="dataset name", type=str, default="FLORIDA")
+    parser.add_argument("-p", "--src_poses_path",
+                        help="path to pipeline's poses", type=str, default="extrinsic.txt")
+    parser.add_argument("-k", "--intrinsic_path",
+                        help="intrinsic_path path", type=str, default="extrinsic.txt")
+    parser.add_argument("-c", "--uncertainty_path",
+                        help="uncertainty ", type=str, default="")
     parser.add_argument("-t", "--gt_folder",
                         help="path to ground truth depth map", type=str)
     parser.add_argument("-o", "--output_path", default="./evaluation", type=str,
@@ -27,25 +29,62 @@ def parse_args():
     args = parser.parse_args()
     return args
 
-def load_poses(path):
+# load poses from underwater dataset
+# def load_poses(path):
+#     f = open(path, "r")
+#     lines = f.readlines()
+#     img_name_poses_map = {}
+#
+#     for line in lines:
+#         tokens = line.split(" ")
+#         timestamp = tokens[0]
+#         timestamp_tokens = timestamp.split(".")
+#         img_name = timestamp_tokens[0]+timestamp_tokens[1]
+#         twc = np.array([np.float64(tokens[1]), np.float64(tokens[2]), np.float64(tokens[3])])
+#         qwc = np.quaternion(np.float64(tokens[7]), np.float64(tokens[4]), np.float64(tokens[5]), np.float64(tokens[6]))
+#         Rwc = quaternion.as_rotation_matrix(qwc)
+#         Rcw = np.transpose(Rwc)
+#         tcw = -Rcw.dot(twc)
+#         img_name_poses_map[img_name] = (Rcw, tcw)
+#     f.close()
+#     return img_name_poses_map
+
+# load poses from Virtual Kitti 2
+def load_poses(path, isLeft):
     f = open(path, "r")
     lines = f.readlines()
     img_name_poses_map = {}
-
-    for line in lines:
+    lines = lines[1:]
+    if isLeft:
+        beg = 0
+    else:
+        beg = 1
+    for i in range(beg, len(lines), 2):
+        line = lines[i].strip()
         tokens = line.split(" ")
-        timestamp = tokens[0]
-        timestamp_tokens = timestamp.split(".")
-        img_name = timestamp_tokens[0]+timestamp_tokens[1]
-        twc = np.array([np.float64(tokens[1]), np.float64(tokens[2]), np.float64(tokens[3])])
-        qwc = np.quaternion(np.float64(tokens[7]), np.float64(tokens[4]), np.float64(tokens[5]), np.float64(tokens[6]))
-        Rwc = quaternion.as_rotation_matrix(qwc)
-        Rcw = np.transpose(Rwc)
-        tcw = -Rcw.dot(twc)
+        frameId = tokens[0]
+        img_name = frameId.zfill(6) #"%06d" % frameId
+        r11, r12, r13 = np.float64(tokens[2]), np.float64(tokens[3]), np.float64(tokens[4])
+        t1, t2, t3 = np.float64(tokens[5]), np.float64(tokens[9]), np.float64(tokens[13])
+        r21, r22, r23 = np.float64(tokens[6]), np.float64(tokens[7]), np.float64(tokens[8])
+        r31, r32, r33 = np.float64(tokens[10]), np.float64(tokens[11]), np.float64(tokens[12])
+
+        Rcw = np.array([[r11, r12, r13], [r21, r22, r23], [r31, r32, r33]])
+        tcw = np.array([t1, t2, t3])
         img_name_poses_map[img_name] = (Rcw, tcw)
     f.close()
     return img_name_poses_map
 
+def load_intrinsic(path):
+    f = open(path, "r")
+    lines = f.readlines()
+    f.close()
+    line = lines[1]
+    line = line.strip()
+    tokens = line.split(" ")
+    fx, fy, cx, cy = np.float64(tokens[2]), np.float64(tokens[3]), np.float64(tokens[4]), np.float64(tokens[5])
+    K = np.array([[fx, 0, cx], [0, fy, cy], [0, 0, 1]])
+    return K
 def projFromMap(map, pose, K): #map save 3D points with world coordinate
     fx, fy, cx, cy = K[0,0], K[1,1], K[0,2], K[1,2]
     for i in range(len(map)):
@@ -112,7 +151,7 @@ def computeU(u, v ,depth, K, baseline, pose):
 
     return U_obs
 
-def read_pipeline_depth_maps(path):
+def read_pfm_depth_maps(path, isKitti2):
     file = open(path, 'rb')
 
     color = None
@@ -147,6 +186,8 @@ def read_pipeline_depth_maps(path):
 
     data = np.reshape(data, shape)
     data = np.flipud(data)
+    if isKitti2:
+        data /= 100
     return data, shape
 
 def write_depth_map(data, fpath, scale=1, file_identifier=b'Pf', dtype="float32"):
@@ -208,20 +249,22 @@ def compare_depth_maps(tgt_folder, depth_map_folder, pipeline_depth_map_names, f
     total_both_no_zeros_pixels = 0
 
     for i in tqdm(range(len(pipeline_depth_map_names)), desc="Loading and comparing raw depth maps", unit="depth maps"):
-        colmap_depth_map_path = tgt_folder + pipeline_depth_map_names[i] + '.png.geometric.bin'
+        depth_name = pipeline_depth_map_names[i]
+        gt_depth_name = "gt_" + depth_name
+        gt_depth_map_path = tgt_folder + gt_depth_name + '.pfm'
         # depth maps
-        colmap_depth_map = read_colmap_depth_maps(colmap_depth_map_path)
+        gt_depth_map, _ = read_pfm_depth_maps(gt_depth_map_path, isKitti2=True)
 
         pipline_depth_map_path = depth_map_folder + pipeline_depth_map_names[i] + '.pfm'
-        pipeline_depth_map, _ = read_pipeline_depth_maps(pipline_depth_map_path)
+        pipeline_depth_map, _ = read_pfm_depth_maps(pipline_depth_map_path, isKitti2=True)
 
-        both_non_zeros = (pipeline_depth_map != 0) & (colmap_depth_map != 0)
-        both_zeros = (pipeline_depth_map == 0) & (colmap_depth_map == 0)
+        both_non_zeros = (pipeline_depth_map != 0) & (gt_depth_map != 0)
+        both_zeros = (pipeline_depth_map == 0) & (gt_depth_map == 0)
         if np.sum(both_non_zeros) == 0:
             continue
-        median = np.median(np.absolute(pipeline_depth_map[both_non_zeros] - colmap_depth_map[both_non_zeros]))
+        median = np.median(np.absolute(pipeline_depth_map[both_non_zeros] - gt_depth_map[both_non_zeros]))
         medians.append(median)
-        mae = np.mean(np.absolute(pipeline_depth_map[both_non_zeros] - colmap_depth_map[both_non_zeros]))
+        mae = np.mean(np.absolute(pipeline_depth_map[both_non_zeros] - gt_depth_map[both_non_zeros]))
         maes.append(mae)
 
         global_min_mae = min(mae, global_min_mae)
@@ -248,6 +291,9 @@ if __name__ == "__main__":
     args = parse_args()
     print(args)
 
+    K = load_intrinsic(args.intrinsic_path)
+    baseline = 0.532725 # in meter
+
     regex_exp = re.compile(r'pfm')
     pipeline_depth_map_paths = [os.path.join(args.src_folder, f) for f in os.listdir(args.src_folder) if regex_exp.search(f)]
     pipeline_depth_map_paths.sort()
@@ -257,32 +303,18 @@ if __name__ == "__main__":
     pipeline_depth_map_names = [i for iterm in pipeline_depth_map_names_2d for i in iterm]
     pipeline_depth_map_names.sort()
 
-    dataset = args.dataset_name
-  
-    # Read depth/normal maps from folder
-    if dataset == "FLORIDA":
-        K = np.array([[595.58148, 0, 380.47882], [0, 593.81262, 302.91428], [0, 0, 1]])
-        baseline = 0.13893691714566
-    elif dataset == "MEXICO":
-        K = np.array([[600.8175, 0, 383.27002], [0, 600.82904, 287.87112], [0, 0, 1]])
-    elif dataset == 'STAVRONIKITA':
-        K = np.array([[1289.2439, 0, 279.92609], [0, 1290.3586, 313.35413], [0, 0, 1]])
-    elif dataset == "REEF":
-        K = np.array([[1289.2439, 0, 279.92609], [0, 1290.9395, 313.69763], [0, 0, 1]])
-    elif dataset == "SPAMIR":
-        K = np.array([[1289.2439, 0, 279.92609], [0, 1290.9395, 313.69763], [0, 0, 1]])
-
-    img_name_SE3 = load_poses(args.src_poses_path)
+    img_name_SE3 = load_poses(args.src_poses_path, isLeft=True)
     assert (len(img_name_SE3) == len(pipeline_depth_map_names))
 
     prev_map = []
     prev_uncertainties = []
 
-    pipeline_depth_map_names = pipeline_depth_map_names[0:10]
+    pipeline_depth_map_names = pipeline_depth_map_names[0:5]
     for i in tqdm(range(0, len(pipeline_depth_map_names)), desc="Start to depth fusion & a build map", unit="depth maps"):
         pipline_depth_map_path = pipeline_depth_map_paths[i]
-        pipeline_depth_map, shape = read_pipeline_depth_maps(pipline_depth_map_path)
-        pose = img_name_SE3[pipeline_depth_map_names[i]]
+        pipeline_depth_map, shape = read_pfm_depth_maps(pipline_depth_map_path, isKitti2=True)
+        frame_id = pipeline_depth_map_names[i][6:]
+        pose = img_name_SE3[frame_id]
 
         height, width = shape[0], shape[1]
         if i == 0:
@@ -323,7 +355,8 @@ if __name__ == "__main__":
                 index = np.argmax(lamda[0])
                 max_eigenval = np.real(lamda[0][index])
 
-                if abs(X_prior[2] - X_obs[2]) > np.sqrt(max_eigenval):
+                dist = np.linalg.norm(X_obs-X_prior)
+                if dist > np.sqrt(max_eigenval):
                     continue
 
                 visited[v_proj, u_proj] = True
@@ -353,12 +386,12 @@ if __name__ == "__main__":
         save_path = args.output_path + "kf_depth_maps/" + pipeline_depth_map_names[i] + ".pfm"
         write_depth_map(pipeline_depth_map, save_path)
 
-    # print("Complete depth map {0}:{1}".format(i, pipeline_depth_map_names[i]))
+    # # print("Complete depth map {0}:{1}".format(i, pipeline_depth_map_names[i]))
 
     # Evaluate depth maps
     compare_depth_maps(args.gt_folder, args.src_folder, pipeline_depth_map_names)
-    fused_depth_folder = args.output_path + "kf_depth_maps/"
-    compare_depth_maps(args.gt_folder, fused_depth_folder, pipeline_depth_map_names, "fused")
+    # fused_depth_folder = args.output_path + "kf_depth_maps/"
+    # compare_depth_maps(args.gt_folder, fused_depth_folder, pipeline_depth_map_names, "fused")
 
     # Pass global_map to Open3D.o3d.geometry.PointCloud and visualize
     # pcd = o3d.geometry.PointCloud()
